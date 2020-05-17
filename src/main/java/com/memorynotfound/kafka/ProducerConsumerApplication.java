3bca3fd9798e923319d0774bf3e46262ac9f7897
tripathi85ashish <65016990+tripathi85ashish@users.noreply.github.com>
2020-05-17 20:49:10 +0530

    first commit

---
+++ src/main/java/com/memorynotfound/kafka/ProducerConsumerApplication.java
@@ -0,0 +1,30 @@
+package com.memorynotfound.kafka;
+
+import com.memorynotfound.kafka.consumer.Receiver;
+import com.memorynotfound.kafka.producer.Sender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducerConsumerApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerConsumerApplication.class, args);
+    }
+
+    @Autowired
+    private Sender sender;
+    
+    @Autowired
+    private Receiver  receiver;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        String data = "Spring Kafka Producer and Consumer Example";
+        sender.send(data);
+    }
+    
+}
